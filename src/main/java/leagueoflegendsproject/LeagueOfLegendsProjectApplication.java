468141583dbc98a8258887eeb244364c90803883
Skilled_T <damian_wieczorek_99@wp.pl>
2023-10-30 02:41:35 +0100

    [PoRo-20] - moved root of the project

---
+++ src/main/java/leagueoflegendsproject/LeagueOfLegendsProjectApplication.java
@@ -0,0 +1,13 @@
+package leagueoflegendsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfLegendsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfLegendsProjectApplication.class, args);
+	}
+
+}
